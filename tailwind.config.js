64a748f62423bc14d85b4a865778142db45a6785
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-07-05 09:57:26 -0500

    Integration: eslint - prettier - tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*{html,js,jsx}'], // purge es reemplazado por content **
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      color: {
+        ...colors,
       },
     },
   },
-  plugins: [],
-}
+};
