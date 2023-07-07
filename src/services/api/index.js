b266712e5ae624958ec072a3dd337503bae46ef4
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-07-07 18:46:48 -0500

    Correct endpoints

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,81 +2,27 @@ const API = process.env.NEXT_PUBLIC_API_URL;
 const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 
 const endpoints = {
-    auth:{
-        login:()=>{
-            `${API}/api/${VERSION}/auth/login`;
-        },
-        profile:()=>{
-            `${API}/api/${VERSION}/auth/profile`;
-
-        }
-    },
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`,
+  },
   products: {
-    getProducts: () => {
-      `${API}/api/${VERSION}/products`;
-    },
-    getProduct: (id) => {
-      `${API}/api/${VERSION}/products/${id}`;
-    },
-    postProducts: () => {
-      `${API}/api/${VERSION}/products`;
-    },
-    putProduct: (id) => {
-      `${API}/api/${VERSION}/products/${id}`;
-    },
-    deleteProduct: (id) => {
-      `${API}/api/${VERSION}/products/${id}`;
-    },
+    getProducts: `${API}/api/${VERSION}/products`,
+    getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    addProducts: `${API}/api/${VERSION}/products`,
+    updateProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    deleteProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
   },
-  users: {
-    getUsers: () => {
-      `${API}/api/${VERSION}/users`;
-    },
-    getUser: (id) => {
-        /api/v1/users/is-available;
-    },
-    postUser: () => {
-      `${API}/api/${VERSION}/users`;
-    },
-    postIsUserAvaible: () => {
-      `${API}/api/${VERSION}/users/is-available`;
-    },
-    putUser: (id) => {
-      `${API}/api/${VERSION}/users${id}`;
-    },
-    deleteUser: (id) => {
-      `${API}/api/${VERSION}/users${id}`;
-    },
+
+  categories: {
+    getCategoriesList: `${API}/api/${VERSION}/categories`,
+    getCategoryItems: (id) => `${API}/api/${VERSION}/categories/${id}/products`,
+    addCategory: `${API}/api/${VERSION}/categories`,
+    updateCategory: (id) => `${API}/api/${VERSION}/categories/${id}`,
   },
-  categories:{
-    getCategories:()=>{
-        `${API}/api/${VERSION}/categories`;
+  files: {
+    addFile: `${API}/api/${VERSION}/files/upload`,
+  },
+};
 
-    },
-    getCategory:(id)=>{
-        `${API}/api/${VERSION}/categories/${id}`;
-
-    },
-    postCategory:()=>{
-        `${API}/api/${VERSION}/categories`;
-
-    },
-    putCategory:(id)=>{
-        `${API}/api/${VERSION}/categories/${id}`;
-
-    },
-    deleteCategory:(id)=>{
-        `${API}/api/${VERSION}/categories/${id}`;
-
-    }},
-   files:{
-    postFile:()=>{
-        `${API}/api/${VERSION}/files/upload`
-    }
-   },
-   getFile:(filename)=>{
-    `${API}/api/${VERSION}/files/${filename}`
-   }
-  }
-
-  export{endpoints}
+export { endpoints };
