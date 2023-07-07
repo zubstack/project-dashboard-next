e3e82ab280e14622afc077d42be9ac137dffb500
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-07-07 12:05:51 -0500

    Log user's input using useRef: [login]

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,17 @@
+import { useRef } from 'react';
 import { FaLock } from 'react-icons/fa';
 
 export default function LoginPage() {
+  const userEmail = useRef(null);
+  const userPassword = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = userEmail.current?.value;
+    const password = userPassword.current?.value;
+
+    console.log(`User: ${email}, Password: ${password}`);
+  };
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -9,7 +20,7 @@ export default function LoginPage() {
             <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -24,6 +35,7 @@ export default function LoginPage() {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
+                  ref={userEmail}
                 />
               </div>
               <div>
@@ -38,6 +50,7 @@ export default function LoginPage() {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  ref={userPassword}
                 />
               </div>
             </div>
