2d072349ece0fa7015bc57373dc8046058e2b181
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-07-07 11:28:27 -0500

    Implement [login] page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,4 +1,4 @@
-import { FaTimes } from 'react-icons/fa';
+import { FaLock } from 'react-icons/fa';
 
 export default function LoginPage() {
   return (
@@ -63,7 +63,7 @@ export default function LoginPage() {
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <FaTimes className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                  <FaLock className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
                 </span>
                 Sign in
               </button>
---
+++ src/pages/login/index.js
@@ -0,0 +1,8 @@
+import LoginPage from '@components/LoginPage';
+import React from 'react';
+
+function Login() {
+  return <LoginPage />;
+}
+
+export default Login;
