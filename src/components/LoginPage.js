b07f60ecb59439161746d565a6169748601b1c41
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-07-14 05:57:37 -0500

    Remake: [login]

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { useRef, useState } from 'react';
 import { FaLock } from 'react-icons/fa';
 import Swal from 'sweetalert2';
+import Logo from '../../public/logo-icon';
 
 export default function LoginPage() {
   const { signIn, user } = useAuth();
@@ -49,12 +50,12 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-full flex items-center justify-center py-20 px-4 sm:px-6 lg:px-8">
+        <div className="absolute top-20">
+          <Logo />
+        </div>
+
         <div className="max-w-md w-full space-y-8">
-          <div>
-            <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-          </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
@@ -68,7 +69,7 @@ export default function LoginPage() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cafe-500 focus:border-cafe-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
                   ref={userEmail}
                 />
@@ -83,7 +84,7 @@ export default function LoginPage() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-cafe-500 focus:border-cafe-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
                   ref={userPassword}
                 />
@@ -92,14 +93,14 @@ export default function LoginPage() {
 
             <div className="flex items-center justify-between">
               <div className="flex items-center">
-                <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+                <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-cafe-600 focus:ring-cafe-500 border-gray-300 rounded" />
                 <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
                   Remember me
                 </label>
               </div>
 
               <div className="text-sm">
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                <a href="#" className="font-medium text-cafe-600 hover:text-cafe-500">
                   Forgot your password?
                 </a>
               </div>
@@ -108,7 +109,7 @@ export default function LoginPage() {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-cafe-600 hover:bg-cafe-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cafe-500"
               >
                 <span className="flex absolute h-4 w-4 top-0 right-0 -mt-1 -mr-1"></span>
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
@@ -119,7 +120,7 @@ export default function LoginPage() {
                       </svg>
                     </>
                   ) : (
-                    <FaLock className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                    <FaLock className="h-5 w-5 text-cafe-500 group-hover:text-cafe-400" aria-hidden="true" />
                   )}
                 </span>
                 Sign in
