b1170776fb26dae1e8b3134da1a272cc144ad6ad
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-02-08 08:01:16 -0500

    linter setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    amd: true,
-    node: true,
-    es6: true,
+    es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
-    "next",
-    "next/core-web-vitals",
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
   rules: {
-    semi: ["error", "alwals"],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 0,
   },
 };
